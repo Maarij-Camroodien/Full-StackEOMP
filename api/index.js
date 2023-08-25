68653739e70c0e0013d52f86b4a3a0972a63bef5
Maarij-Camroodien <maarijcamroodien18@gmail.com>
2023-08-25 10:04:01 +0200

    authenticate

--- api/index.js
+++ api/index.js
@@ -5,6 +5,16 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const errorHandling = require("./middleware/Authenticate");
 const port = +process.env.PORT || 3000
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Request-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Expose-Headers", "Authorization");
+  next();
+});
  
 app.use(express.static('../static'))
 
